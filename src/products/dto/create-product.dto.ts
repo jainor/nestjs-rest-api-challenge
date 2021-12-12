b236e5a0ca110e384113f9bce419730bfcd815bb
jainor <jainorcardenas@gmail.com>
2021-12-12 16:59:31 -0500

    feat: add products service

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,45 @@
-export class CreateProductDto {}
+import { Expose, Exclude } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  IsNumber,
+  IsPositive,
+  IsDecimal,
+} from 'class-validator';
+
+@Exclude()
+export class CreateProductDto {
+  @Expose()
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  readonly id: number;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @Expose()
+  @IsString()
+  readonly description: string;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  readonly categoryId: number;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsNumber()
+  @IsDecimal()
+  readonly price: number;
+
+  @Expose()
+  @IsNumber()
+  @IsPositive()
+  readonly stock: number;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const { id } = createProductDto;
+
+    if (await this.prisma.product.count({ where: { id } })) {
+      throw new HttpException('id already used', HttpStatus.BAD_REQUEST);
+    }
+
+    return this.prisma.product.create({ data: createProductDto });
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.prisma.product.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.prisma.product.findUnique({
+      where: { id: id },
+    });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    return this.prisma.product.update({
+      data: updateProductDto,
+      where: {
+        id,
+      },
+    });
   }
 
   remove(id: number) {
