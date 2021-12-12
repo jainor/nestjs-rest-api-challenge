e416cab7684bb9c9ea328a7b0b8f0526eeab74c7
jainor <jainorcardenas@gmail.com>
2021-12-12 10:47:15 -0500

    feat: add basic prisma queries

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -13,7 +16,9 @@ export class UsersService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.user.findUnique({
+      where: { id: id },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
