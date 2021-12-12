2e51d234c06b24db7795e386cec3760410e08071
jainor <jainorcardenas@gmail.com>
2021-12-12 14:40:09 -0500

    feat: user get/create endpoint

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+import { Expose, Exclude } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  IsNumber,
+} from 'class-validator';
+
+@Exclude()
+export class CreateUserDto {
+  @Expose()
+  @IsNotEmpty()
+  @IsNumber()
+  readonly id: number;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  readonly firstName: string;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  readonly lastName: string;
+
+  @Expose()
+  @IsEmail()
+  readonly email: string;
+
+  @Expose()
+  @IsString()
+  @Length(6, 200)
+  readonly password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const { id } = createUserDto;
+
+    if (await this.prisma.user.count({ where: { id } })) {
+      throw new HttpException('id already used', HttpStatus.BAD_REQUEST);
+    }
+
+    return this.prisma.user.create({ data: createUserDto });
   }
 
   findAll() {
