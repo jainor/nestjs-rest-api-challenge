f5e9d2e8c75af2c6a4a4639ce4fc3dd6704f8e54
jainor <jainorcardenas@gmail.com>
2021-12-12 16:41:33 -0500

    feat: add categories service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const { id } = createCategoryDto;
+
+    if (await this.prisma.category.count({ where: { id } })) {
+      throw new HttpException('id already used', HttpStatus.BAD_REQUEST);
+    }
+
+    return this.prisma.category.create({ data: createCategoryDto });
   }
 
   findAll() {
-    return `This action returns all categories`;
+    return this.prisma.category.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    return this.prisma.category.findUnique({
+      where: { id: id },
+    });
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  update(id: number, updateCategorieDto: UpdateCategoryDto) {
+    return this.prisma.category.update({
+      data: updateCategorieDto,
+      where: {
+        id,
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} category`;
+    return `This action removes a #${id} categorie`;
   }
 }
--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,29 @@
-export class CreateCategoryDto {}
+import { Expose, Exclude } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  IsNumber,
+  IsPositive,
+  IsDecimal,
+} from 'class-validator';
+
+@Exclude()
+export class CreateCategoryDto {
+  @Expose()
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  readonly id: number;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @Expose()
+  @IsString()
+  readonly description: string;
+
+}
