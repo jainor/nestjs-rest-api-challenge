76359a9461cb67275f239f65db54fd41910fa2c2
jainor <jainorcardenas@gmail.com>
2021-12-12 16:01:06 -0500

    chore: add boilerplate resource products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, ProductsModule],
   controllers: [],
   providers: [],
 })
