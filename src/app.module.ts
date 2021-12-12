e3b412d5435ca6a64941bd349b7232a974368ca5
jainor <jainorcardenas@gmail.com>
2021-12-12 16:27:37 -0500

    chore: add boilerplate resource categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, ProductsModule],
+  imports: [UsersModule, PrismaModule, ProductsModule, CategoriesModule],
   controllers: [],
   providers: [],
 })
