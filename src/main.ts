8bf0ac10c9e3824c4364063817a8d2f5cf5854e3
Jainor Nestor Cardenas Choque <jainorcardenas@Jainors-MacBook-Pro.local>
2022-08-17 15:35:12 -0500

    feat: update buildspec aws

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 80);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
